1570f45dd8034eaaa03a4321ffa6b6b63d6172bf
Vladimir Malashkin <faiver90@gmail.com>
2024-09-03 12:46:13 +0300

    up

--- app_v0/celery.py
+++ app_v0/celery.py
@@ -2,18 +2,11 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-# Задайте переменную окружения для настроек Django
 from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app_v0.settings')
-
-# Создайте экземпляр Celery
 celery_app = Celery('app_v0')
-
-# Используйте конфигурацию из настроек Django, используя префикс 'CELERY'
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# Автоматическое обнаружение задач в ваших приложениях Django
 celery_app.autodiscover_tasks()
 
 CELERY_BEAT_SCHEDULE = {
