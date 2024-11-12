6c5ceee38d522b856667dfbe5e3db57b1926ad59
Vladimir Malashkin <faiver90@gmail.com>
2024-11-12 12:33:00 +0200

    add collet status

--- apps/questionnaire/services/constants.py
+++ apps/questionnaire/services/constants.py
@@ -1,7 +1,11 @@
-OFFER_STATUSES = [
-    'Ошибка',
-    'Ожидание решения',
-    'Отказ',
-    'Запрос доп информации',
-    'Одобрение'
-]
+import logging
+
+from apps.core.banking_services.sovcombank.sovcombank_services.sovcombank_endpoints_servicves.sovcombank_get_status.sovcombank_get_status import \
+    SovcombankGetStatusSendHandler
+
+logger = logging.getLogger(__name__)
+
+sovcombank_get_statusSend_handler = SovcombankGetStatusSendHandler()
+collect_sovcom_status = sovcombank_get_statusSend_handler.description_list.keys()
+
+COLLECTIONS_OFFER_STATUSES = list(collect_sovcom_status)
--- apps/questionnaire/services/offer_services/get_offers_by_status.py
+++ apps/questionnaire/services/offer_services/get_offers_by_status.py
@@ -1,10 +1,13 @@
+import logging
 from typing import List, Dict
 
 from django.shortcuts import get_list_or_404
 from django.template.loader import render_to_string
 
 from apps.questionnaire.models import SelectedClientOffer
-from apps.questionnaire.services.constants import OFFER_STATUSES
+from apps.questionnaire.services.constants import COLLECTIONS_OFFER_STATUSES
+
+logger = logging.getLogger(__name__)
 
 
 class GetByStatusOfferService:
@@ -14,14 +17,13 @@ class GetByStatusOfferService:
 
     def __init__(self):
         # Инициализируем offers_data на основе статусов из константы
-        self.offers_data = {status: [] for status in OFFER_STATUSES}
+        self.offers_data = {status: [] for status in COLLECTIONS_OFFER_STATUSES}
 
     def get_offers_by_status(self, client_id) -> Dict[str, list]:
         offers = get_list_or_404(SelectedClientOffer, client_id=client_id)
-
         for offer in offers:
             status = offer.status_select_offer
-            if status and status in OFFER_STATUSES:  # Проверяем наличие статуса в константе
+            if status and status in COLLECTIONS_OFFER_STATUSES:  # Проверяем наличие статуса в константе
                 offer_data = render_to_string('questionnaire/card_offer.html', {
                     'offer': offer,
                     'client_id': client_id,
