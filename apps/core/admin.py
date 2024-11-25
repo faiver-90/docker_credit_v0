2e1e669cdcededb1fb9f90c1f794fea362bfd02c
Vladimir Malashkin <faiver90@gmail.com>
2024-11-25 22:59:54 +0200

    upd calculator

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from apps.core.models import Event
+from apps.core.models import Event, ResponseCalculationSovComBank
 
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = ('event_id', 'aggregate_id', 'event_type', 'payload', 'timestamp')
     search_fields = ('event_id', 'aggregate_id', 'event_type', 'payload', 'timestamp')
+
+
+@admin.register(ResponseCalculationSovComBank)
+class ResponseCalculationSovComBankAdmin(admin.ModelAdmin):
+    list_display = ('request_id', 'dealer_id')
+    search_fields = ('request_id', 'dealer_id')
--- apps/core/models.py
+++ apps/core/models.py
@@ -42,7 +42,7 @@ class OffersSovComBank(models.Model):
 # Модели для калькулятора СовКомБанк
 class ResponseCalculationSovComBank(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    request_id = models.CharField(max_length=255)
+    request_id = models.UUIDField(default=uuid.uuid4)
     dealer_id = models.CharField(max_length=255)
 
 
